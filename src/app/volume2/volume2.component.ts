c009efbd939de3d86cb9f56957a2c7f3ff2d8fab
Plawan Rath <plawanrath@gmail.com>
2018-04-26 06:40:00 -0400

    updating vollume views and components

--- src/app/volume2/volume2.component.ts
+++ src/app/volume2/volume2.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Volume2Component implements OnInit {
 
+  articleNo: number = 1;
+  NoOfArticles: number = 6;
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+  nextArticle() {
+    if(this.articleNo < this.NoOfArticles) {
+      this.articleNo++;
+    }
+  }
+
+  previousArticle() {
+    if(this.articleNo > 1) {
+      this.articleNo--;
+    }
+  }}
--- src/app/volume4/volume4.component.ts
+++ src/app/volume4/volume4.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Volume4Component implements OnInit {
 
+  articleNo: number = 1;
+  NoOfArticles: number = 1;
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+  nextArticle() {
+    if(this.articleNo < this.NoOfArticles) {
+      this.articleNo++;
+    }
+  }
+
+  previousArticle() {
+    if(this.articleNo > 1) {
+      this.articleNo--;
+    }
+  }}
